ebf329b6d989c1bda0e10d198007f37472b53bec
Rackon13 <tatow2001@gmail.com>
2022-03-21 17:04:23 -0300

    up

--- index.js
+++ index.js
@@ -23,13 +23,18 @@ function invert(people){
  * @returns Uma média a partir do array de notas
  */
 function mean(grades){
-
+    if(grades.lenght===0){return 0;}
+    var x = 0;
+    grades.forEach(element => {
+        x += element;
+    });
+    
     // TODO
     // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
     // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
     // ou superior a 7.
 
-    return 0;
+    return x/grades.lenght;
 }
 
 /**
@@ -40,12 +45,13 @@ function mean(grades){
  */
 function isApproved(mean){
 
+    if(mean>=7){return "aprovado"}
     // TODO
     // 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
     // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
     // ou superior a 7.
 
-    return "";
+    return "reprovado";
 }
 
 /**
@@ -55,14 +61,18 @@ function isApproved(mean){
  * @returns Uma descrição da data informada
  */
 function wide(strDate){
-
+    if(!("/" in strDate)){return "";}
+    meses =["janeiro", "fevereiro", "março", "abril", "Maio", "junho", "agosto", "outubro", "novembro", "dezembro"]
+    l = strDate.split("/");
+    l[1] = meses[Number(l[1])-1];
+    f = `${l[0]} de ${l[1]} de ${l[2]}`;
     // TODO
     // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
     // por exemplo, de "03/03/2022" para "03 de março de 2022". Dica: use a função
     // “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
     // Nesse caso, o separador é a barra (/) da data.
 
-    return "";
+    return f;
 }
 
 // Trecho utilizado pelos testes
